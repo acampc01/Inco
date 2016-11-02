142a7e4632cbffc5690e44ffca15cd43dec90233
acampc01 <acampc01@estudiantes.unileon.es>
2016-11-02 14:11:23 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,28 @@
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
+	
+	private static ArrayList <String> listaClases;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Operaciones op = new Operaciones();
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+		listaClases = new ArrayList <String>();
+		
+		for(int i=0;i<2;i++){
+			nuevaOperacion();
+			usarOperacion();
+		}
 	}
+	
+	private static void usarOperacion() throws ClassNotFoundException, InstantiationException, IllegalAccessException{
+		Class c = Class.forName(listaClases.get(0));
+		c.newInstance();
+	}
+
+	private static void nuevaOperacion() throws IOException{
+		NuevaOperacion nueva = new NuevaOperacion();
+		listaClases.add(nueva.getNombreClass());
+	}
+	
 
 }
