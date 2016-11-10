c7802c5ea7bd9ae8671504366a25f2c8bd784870
acampc01 <acampc01@estudiantes.unileon.es>
2016-11-10 11:19:55 +0100

    Update Operaciones.java

--- Operaciones.java
+++ Operaciones.java
@@ -1,4 +1,7 @@
+
+import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
 public class Operaciones{
@@ -8,15 +11,24 @@ public class Operaciones{
 	/**
 	 * Constructor de la clase
 	 * @param nombre
-	 * @throws IOException
+	 * @throws Exception 
 	 * @throws ClassNotFoundException
 	 * @throws InstantiationException
 	 * @throws IllegalAccessException
 	 */
-	public Operaciones() throws IOException{
+	public Operaciones() throws Exception{
 		listaClases = new ArrayList <String>();
+		
 		nuevaOperacion("Suma");
 		nuevaOperacion("Repetir");
+		
+		File[]	archivosDisponibles = new File("./src/").listFiles();
+		for (int i = 0; i < archivosDisponibles.length; i++) {
+			String aux = archivosDisponibles[i].getName().toString();
+			aux = aux.split(".")[0];
+			System.out.println(aux);
+		}
+		
 	}
 
 	/**
@@ -24,10 +36,13 @@ public class Operaciones{
 	 * @throws ClassNotFoundException
 	 * @throws InstantiationException
 	 * @throws IllegalAccessException
+	 * @throws SecurityException 
+	 * @throws InvocationTargetException 
+	 * @throws IllegalArgumentException 
 	 */
-	public static void usarOperacion() throws ClassNotFoundException, InstantiationException, IllegalAccessException{
+	public static int usarOperacion() throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, SecurityException{
 		Class c = Class.forName(listaClases.get(0));
-		c.newInstance();
+		return (int)c.getMethods()[0].invoke(c,2,2);
 	}
 
 	/**
@@ -39,6 +54,9 @@ public class Operaciones{
 		CrearArchivo nueva = new CrearArchivo(nombre);
 		listaClases.add(nueva.getNombreClass());
 	}
-
+	
+	public static ArrayList <String> getListaMetodos(){
+		return listaClases;
+	}
 
 }
